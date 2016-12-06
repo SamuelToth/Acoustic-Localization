a49efcaf8e182d0582a686caafd30c2ce164801e
Brian Bauer <ucn2510@n0691.ten.osc.edu>
2016-12-06 13:51:57 -0500

    distances.cu done, but not working

--- distances.cu
+++ distances.cu
@@ -1,10 +1,10 @@
-#include "../paracompFinalConstants/constants.cu"
+#include "constants.cu"
 #include <math.h>
 
 //numFFTPairs is number of mics which have a set of matches
 __global__
-void findCoordinate(int numFFTPairs, int numMics, Coordinate* micCoordinates,
-                    WavePairContainer* filteredPairs) {
+void findCoordinate(int numFFTPairs, Coordinate* micCoordinates,
+                    WavePairContainer* filteredPairs, Coordinate* newPoint) {
 
   int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -20,14 +20,36 @@ void findCoordinate(int numFFTPairs, int numMics, Coordinate* micCoordinates,
       int mic2 = filteredPairs[tid].secondFFT;
       decimal pairDistance = sqrt(pow(micCoordinates[mic2].x - micCoordinats[mic1].x ,2)+pow(micCoordinates[mic2].y-micCoordinates[mic1].y,2));
       //phase^2 = (x-x1)^2 + (y-y1)^2
-      if(filteredPairs[tid].firstFFT.offset + filteredPairs[tid].secondFFT.offset < pairDistance || //if dist > r1+r2
-         fabs(filteredPairs[tid].firstFFT.offset - filteredPairs[tid].secondFFT.offset) > pairDistance || //if dist < |r1-r2|
-         (pairDistance==0 && filteredPairs[tid].firstFFT.offset==filteredPairs[tid].secondFFT.offset)) //if (dist==0 && r1==r2)
-         return;
+      decimal mic1r=filteredPairs[tid].firstFFT.offset;
+      decimal mic2r=filteredPairs[tid].secondFFT.offset;
       
-             
+      
+      if(mic1r + mic2r < pairDistance || //if dist > r1+r2
+         fabs(mic1r - mic2r) > pairDistance || //if dist < |r1-r2|
+         (pairDistance==0 && mic1r==mic2)) //if (dist==0 && r1==r2)
+        return;
+      
+      decimal a = (mic1r*mic1r - mic2r*mic2r + pairDistance*pairDistance)/(2*pairDistance);
+      decimal altitude = sqrt(mic1r*mic1r - a*a);
 
-  
+      Coordinate subPoint = {micCoordinates[mic2].x - micCoordinates[mic1].x,
+                             micCoordinates[mic2].y - micCoordinates[mic1].y}
+      
+      subPoint.x = subPoint.x*(a/pairDistance);
+      subPoint.y = subPoint.y*(a/pairDistance);
+      subPoint.x = subPoint.x+micCoordinates[mic2].x;
+      subPoint.y = subPoint.y+micCoordinates[mic2].y;
+    
+      decimal x3,y3,x4,y4;
+      x3 = subPoint.x + altitude*(micCoordinates[mic2].y-micCoordinates[mic1].y)/pairDistance;
+      y3 = subPoint.y - altitude*(micCoordinates[mic2].x-micCoordinates[mic1].x)/pairDistance;
+      x4 = subPoint.x - altitude*(micCoordinates[mic2].y-micCoordinates[mic1].y)/pairDistance;
+      y4 = subPoint.y + altitude*(micCoordinates[mic2].x-micCoordinates[mic1].x)/pairDistance;
+
+      *newPoint = subPoint;
+    }
+  }
+}
   /*
   decimal micDistances[numFFTPairs];
 
